a25556cc86e71a817b4b84e68e0ecdb71eac4187
Beatriz <bcristinarodrigues@gmail.com>
2022-03-28 13:23:57 -0300

    fix: fix function

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/teste', methods=['POST'])
 def teste():
-    json_object = request.json()
+    json_object = request.json
     return json_object, 200
 
 if __name__ == "__main__":
